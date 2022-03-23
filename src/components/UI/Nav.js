f3cd57f77856ec94259ca34f37977ac44dfaf090
Oluwadamilola Gbotolorun <damigbot@gmail.com>
2022-03-23 22:50:15 +0100

    Update page title

--- src/components/UI/Nav.js
+++ src/components/UI/Nav.js
@@ -3,7 +3,11 @@ import React from "react";
 import "./Nav.css";
 
 const Nav = () => {
-	return <div className="Nav"></div>;
+	return (
+		<div className="Nav">
+			<h1 style={{ color: "white", fontSize: "4rem" }}>React Job Board</h1>
+		</div>
+	);
 };
 
 export default Nav;
